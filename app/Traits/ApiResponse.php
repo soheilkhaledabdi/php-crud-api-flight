700dc912a57859be2def93a41d3fc9573bf63901
soheil khaledabadi <soheilkhaledabdi@gmail.com>
2024-08-29 13:44:14 +0330

    Update -> use flight json to return json

--- app/Traits/ApiResponse.php
+++ app/Traits/ApiResponse.php
@@ -2,26 +2,25 @@
 
 namespace App\Traits;
 
+use Flight;
+
 trait ApiResponse
 {
-    public function success($data, $message = 'Success', $code = 200)
+    public function success(array $data, string $message = 'Success', int $code = 200)
     {
-        return $this->jsonResponse($data, true, $message, $code);
-    }
-
-    public function failed($data = null, $message = 'Fail', $code = 500)
-    {
-        return $this->jsonResponse($data, false, $message, $code);
-    }
-
-    private function jsonResponse($data, $status, $message, $code)
-    {
-        http_response_code($code);
-        header('Content-Type: application/json');
-        echo json_encode([
-            'result' => $data,
-            'status' => $status,
+        return Flight::json([
+            'data' => $data,
+            'status' => $code,
             'message' => $message,
-        ]);
+        ], $code);
+    }
+
+    public function failed(?array $data = null, string $message = 'Fail', int $code = 500)
+    {
+        return Flight::json([
+            'data' => $data,
+            'status' => $code,
+            'message' => $message,
+        ], $code);
     }
 }
