ef88e18beb8885ea2c98ad60761e4196bdba9943
soheil khaledabadi <soheilkhaledabdi@gmail.com>
2024-09-20 18:29:50 +0330

    Update -> Add env function to retrieve environment variables

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -34,3 +34,10 @@ function getMessage(string $key, array $params = []): string
 
     return $message;
 }
+
+function env(string $key,string $default = null)
+{
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../..');
+    $dotenv->load();
+    return $_ENV[$key] ?? $default;
+}
